f36303052b256828c0e8407ba76b2a3f06b20816
ajs <140010746+aboodJS@users.noreply.github.com>
2026-06-26 19:05:56 +0300

    added cards to display the data, the server now fetchs and returns data to the client

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,50 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import DayBox from "./components/DayBox.vue";
+
+async function GetDataFromServer(name) {
+  try {
+    await fetch("http://localhost:3000", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ query: name.toLowerCase() }),
+    })
+      .then((res) => res.json())
+      .then((d) => (results.value = d));
+  } catch (error) {
+    console.log(error);
+  }
+  console.log(query.value);
+}
+const results = ref();
+const query = ref("");
+</script>
 
 <template>
   <nav class="flex justify-between w-[90vw] mx-auto py-3 items-center">
     <h3>ajs weather</h3>
     <div>
-      <input class="w-76" type="text" placeholder="enter the full name of a city eg. 'amman'" />
-      <button>Search</button>
+      <input
+        v-model="query"
+        class="w-76"
+        type="text"
+        placeholder="enter the full name of a city eg. 'amman'"
+      />
+      <button @click="() => GetDataFromServer(query)">Search</button>
     </div>
   </nav>
+  <main>
+    <section class="flex gap-6 justify-evenly items-center h-screen" v-if="results !== undefined">
+      <DayBox
+        v-for="condition in results.daily.data"
+        :date="condition.day"
+        :weather="condition.weather"
+        :summary="condition.summary"
+      ></DayBox>
+    </section>
+  </main>
 </template>
 
 <style scoped></style>
---
+++ src/components/DayBox.vue
@@ -0,0 +1,11 @@
+<script setup>
+const props = defineProps(["date", "weather", "summary"]);
+</script>
+
+<template>
+  <div class="text-center grid text-sm h-56 w-38 bg-blue-500 rounded">
+    <p>{{ props.date }}</p>
+    <p>{{ props.weather }}</p>
+    <p>{{ props.summary }}</p>
+  </div>
+</template>
