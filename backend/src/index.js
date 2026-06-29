074c0a68061e63dc2cec855c7a4b1edb9781b09d
ajs <140010746+aboodJS@users.noreply.github.com>
2026-06-29 16:10:06 +0300

    started working on error handeling

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,16 +8,20 @@ app.use(express.json());
 app.use(express.urlencoded());
 
 app.post("/", async (req, res) => {
-  try {
-    const data = await fetch(
-      `https://www.meteosource.com/api/v1/free/point?place_id=${req.body.query}&sections=daily&key=${process.env.API_KEY}`,
-    )
-      .then((res) => res.json())
-      .then((d) => d);
-    res.json(data);
-  } catch (error) {
-    res.error(error);
-  }
+  const data = await fetch(
+    `https://www.meteosource.com/api/v1/free/point?place_id=${req.body.query}&sections=daily&key=${process.env.API_KEY}`,
+  )
+    .then((response) => {
+      console.log(response.status);
+      if (response.status !== 200) {
+        res.json({ error: "something went wrong" });
+      }
+      return response.json();
+    })
+    .then((d) => {
+      return res.json(d);
+    })
+    .catch((d) => res.error(d));
 });
 
 app.listen(3000, () => {
--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,30 @@ import { ref } from "vue";
 import DayBox from "./components/DayBox.vue";
 
 async function GetDataFromServer(name) {
-  try {
-    await fetch("http://localhost:3000", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ query: name.toLowerCase().split(" ").join("-") }),
+  await fetch("http://localhost:3000", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ query: name.toLowerCase().split(" ").join("-") }),
+  })
+    .then((res) => {
+      console.log(res.status);
+      return res.json();
     })
-      .then((res) => res.json())
-      .then((d) => (results.value = d));
-  } catch (error) {
-    console.log(error);
-  }
-  console.log(query.value);
+    .then((d) => {
+      results.value = d;
+      isError.value = false;
+    })
+    .catch((e) => {
+      isError.value = true;
+      results.value = e;
+    });
 }
 const results = ref();
 const query = ref("");
+const isError = ref(false);
+const erroVal = ref();
 </script>
 
 <template>
@@ -45,7 +52,7 @@ const query = ref("");
   <main>
     <section
       class="grid grid-cols-4 grid-rows-2 gap-2 justify-center content-center h-screen max-md:grid-rows-3 max-md:grid-cols-3 max-md:gap-4"
-      v-if="results !== undefined"
+      v-if="isError === false && results !== undefined"
     >
       <DayBox
         v-for="condition in results.daily.data"
@@ -56,6 +63,7 @@ const query = ref("");
         :full-data="condition"
       ></DayBox>
     </section>
+    <section v-else-if="results !== undefined && isError === true">{{ results }}</section>
   </main>
 </template>
 
